5e11240ff1cad53827801da9033442a632f4e8d3
Seven-112 <greensatellite777@gmail.com>
2022-04-07 11:53:29 +0700

    build source

--- dist/modules/nft/userTicket.interface.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../user/user.interface';
-import { Event } from './event.interface';
-export declare class UserTicket {
-    id: string;
-    event: Event;
-    user: User;
-    number_ticket: number;
-    constructor();
-}
