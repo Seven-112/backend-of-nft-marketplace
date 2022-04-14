0abf57f0af28591733750a94db8895636734dee9
Seven-112 <greensatellite777@gmail.com>
2022-04-14 19:59:04 +0700

    fix bug create event

--- dist/middlewares/logger.middelware.js
+++ dist/middlewares/logger.middelware.js
@@ -1,32 +1,22 @@
-'use strict';
-var __decorate =
-  (this && this.__decorate) ||
-  function (decorators, target, key, desc) {
-    var c = arguments.length,
-      r =
-        c < 3
-          ? target
-          : desc === null
-          ? (desc = Object.getOwnPropertyDescriptor(target, key))
-          : desc,
-      d;
-    if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
-      r = Reflect.decorate(decorators, target, key, desc);
-    else
-      for (var i = decorators.length - 1; i >= 0; i--)
-        if ((d = decorators[i]))
-          r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.LoggerMiddleware = void 0;
-const common_1 = require('@nestjs/common');
-let LoggerMiddleware = class LoggerMiddleware {
-  use(req, res, next) {
-    console.log(req.path, req.body);
-    next();
-  }
 };
-LoggerMiddleware = __decorate([(0, common_1.Injectable)()], LoggerMiddleware);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoggerMiddleware = void 0;
+const common_1 = require("@nestjs/common");
+let LoggerMiddleware = class LoggerMiddleware {
+    use(req, res, next) {
+        console.log('Request...');
+        console.log(req.path, req.body);
+        next();
+    }
+};
+LoggerMiddleware = __decorate([
+    (0, common_1.Injectable)()
+], LoggerMiddleware);
 exports.LoggerMiddleware = LoggerMiddleware;
 //# sourceMappingURL=logger.middelware.js.map
